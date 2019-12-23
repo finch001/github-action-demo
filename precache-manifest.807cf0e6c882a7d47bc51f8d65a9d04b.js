20628073880df4792782fa9eaad44a8e01653fb6
finch001 <422465778@qq.com>
2019-12-23 06:42:57 +0000

    Deploying to gh-pages from master 5d780cc481a2b10ca0ba2514f641c6628c45a3f2

---
+++ precache-manifest.807cf0e6c882a7d47bc51f8d65a9d04b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb78d99b78b3c2dad90ad9630691d075",
+    "url": "/github-action-demo/index.html"
+  },
+  {
+    "revision": "95b00d253fc228470863",
+    "url": "/github-action-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "af594028a5e8a394766e",
+    "url": "/github-action-demo/static/js/2.c6825b4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-action-demo/static/js/2.c6825b4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95b00d253fc228470863",
+    "url": "/github-action-demo/static/js/main.cfc1f00b.chunk.js"
+  },
+  {
+    "revision": "e7c2d083278df5856202",
+    "url": "/github-action-demo/static/js/runtime-main.a77c8c60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-action-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/github-action-demo/precache-manifest.807cf0e6c882a7d47bc51f8d65a9d04b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-action-demo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
